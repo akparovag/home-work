fa35cff6b5a7dca614ee61c21ec3e7cdf3eef5e3
akparovag <78397054+akparovag@users.noreply.github.com>
2021-02-17 19:28:16 +0600

    Add files via upload

---
+++ решение задачь из практики 3,4.py
@@ -0,0 +1,71 @@
+if 1: #3практика
+    print('hello 1')
+
+a=3
+if a==3:
+    print('здесь а=', a)
+
+if a>1:
+    print("hello 3")
+
+ist = [1,2,3]
+if ist:
+    print('hello 4')
+
+a=3
+if a>2:
+    print('h')
+else:
+    print('l')
+
+a=1
+if a>2:
+    print('h')
+else:
+    print('l')
+
+a=17
+b= True if a>10 else False
+print(b)
+
+a=int(intput('введите число:'))
+if a<0
+    print('neg')
+elif a == 0:
+    print('zero')
+else:
+    print('pos')
+
+a=0  #4практика
+while a<7:
+    print('a')
+    a+=1
+
+a=0
+while a==0:
+    print('a')
+
+a=0
+while a>=0:
+    if a==7:
+        break
+    a+=1
+    print('a')
+
+a=-1
+while a<10
+    a+=1
+    if a>7:
+        continue
+    print('a')
+
+for i in range(5):
+    print('hello')
+
+ist = [1,3,5,7,9]
+for i in ist:
+    print(i**2)
+
+word_str = "hello, world!"
+for g in word_str:
+    print(g)
