3e8d318f38e6811017a955deef9f550f7cebd89f
akparovag <78397054+akparovag@users.noreply.github.com>
2021-02-17 19:04:22 +0600

    Add files via upload

---
+++ регшение задачь из практики 2.py
@@ -0,0 +1,44 @@
+import math #3 задачи из практики 
+x=10
+x=x**5/3
+print(x)
+
+import math #4
+x=5
+y=2
+z=math.cos(x+y/2) 
+print(z)
+
+x=2 #7
+a=math.log2(x*2)+ math.log10(x)
+print(a)
+
+
+b=math.atan(x)+y**5 / math.sin(20) #14
+print(b)
+
+x=5 #17
+d=math.log2(x)+x**1/3  
+print(d)
+
+
+import math #19
+a=4
+x=2
+b= math.sqrt(a) - abs( math.e **math.log(x))
+print(b)
+
+import math #22
+x=6
+y=(math.log(abs(x-15))) + (x**4/5) + (math.e**3/3) + 4**1/3
+print(y)
+
+import math #25
+x=5
+y=(x**5) + math.log(math.sqrt(math.pi+ abs(5-x)))
+print(y)
+
+import math #26
+x=4
+y=(x**7) + x/2+x/12+ math.e**5
+print(y)
